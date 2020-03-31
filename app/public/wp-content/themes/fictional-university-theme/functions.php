4fcbc2ba616c03013f96c2566a23ea23aa7ceafb
Matt <271films@gmail.com>
2020-03-31 05:27:55 -0400

    pluginUpdate. Fixed query in functions.php

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -2,7 +2,7 @@
 
 function university_files() {
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
-  // wp_enqueue_script('bible-reading-plan', 'https://bibles.org/static/widget/v2/widget.js', NULL, '2.0', true);
+  wp_enqueue_script('bible-reading-plan', 'https://bibles.org/static/widget/v2/widget.js', NULL, '2.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_stylesheet_uri());
@@ -17,7 +17,9 @@ function university_features() {
 add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query) {
-  $query->set('post_per_page', '1');
+    if (!is_admin() AND is_post_type_archive('bible-plan') AND $query->is_main_query()) {
+      $query->set('posts_per_page', '1');
+    }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
